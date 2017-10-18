73bec090266846b77eb82e058e5bb7741d7b75fb
Michael Cassidy <mikecassidy0000@gmail.com>
2017-10-18 16:14:05 -0400

    new diggs

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def work
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/work'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+root to: 'pages#home'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
